4c1bc564346e449b152c5aaa48f1a7d797d33817
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-17 15:16:59 -0400

    fetched coffees from api and created iced & hot buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import CCoffee from "./components/CCoffee";
+// import CCoffee from "./components/CCoffee";
 import FCoffee from "./components/FCoffee";
 
 function App() {
   return (
     <>
-    <CCoffee firstName='Stephanie' />
+    {/* <CCoffee firstName='Stephanie' /> */}
     <FCoffee firstName='Stephanie' />
     </>
   );
