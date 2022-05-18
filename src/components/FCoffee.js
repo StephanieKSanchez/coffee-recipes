55addb044380359d3b08033a48988a34a25695c1
Stephanie Sanchez <stephaniesanchez@StephaniesAir2.hsd1.fl.comcast.net>
2022-05-17 21:10:19 -0400

    added comments

--- src/components/FCoffee.js
+++ src/components/FCoffee.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-function FCoffee({ firstName }) {
-    const [coffeeList, setCoffeeList] = useState();
+function FCoffee({ firstName }) { // defining functional React component taking in a prop called firstName
+    const [coffeeList, setCoffeeList] = useState(); // creating state variable and setter and setting the state initially to undefined
     const [temperature, setTemperature] = useState('iced');
-    useEffect(()=>{
-        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-        .then(response => response.json())
-        .then(data => setCoffeeList(data))
-        .catch(console.error)
-    },[temperature])
-    return (
-        <>
-        <h1>Coffee List (F)</h1>
-        <p>Hello {firstName} </p>
+    useEffect(()=>{ // runs initially & when temperature changes
+        fetch(`https://api.sampleapis.com/coffee/${temperature}`) //goes to api point & get coffee
+        .then(response => response.json()) //calling json method on the response and waiting to get it
+        .then(data => setCoffeeList(data)) 
+        .catch(console.error) // console log errors from lines 7 through 9
+    },[temperature]) // dependency list 
+    return ( //React always returns valid jsx
+        <> 
+        <h1>Coffee List (F)</h1> 
+        <p>Hello {firstName} </p> 
         <button onClick={()=>setTemperature('hot')}>HOT</button>
         <button onClick={()=>setTemperature('iced')}>ICED</button>
         {!coffeeList
